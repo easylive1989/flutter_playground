4bc355efb9249fcaf50f02b26f1892a9bf695c7f
Paul <easylive1989@gmail.com>
2022-05-01 15:36:36 +0800

    update route code

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_playground/dependencies/http/web_content_widget.dart';
@@ -25,42 +24,32 @@ void main() {
   // drawLogin();
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  Map<String, WidgetBuilder> _routeMap = {};
-
-  @override
-  void initState() {
-    super.initState();
-    _routeMap["/Bottom_Scroll"] = (context) => BottomScrollExample();
-    _routeMap["/Column_Flex"] = (context) => ColumnFlex();
-    _routeMap["/KeyListWidget"] = (context) => KeyListWidget();
-    _routeMap["/FloatingWidget"] = (context) => OverlayFloatingWidget();
-    _routeMap["/Get"] = (context) => GetCountView();
-    _routeMap["/Notification"] = (context) => MyNotificationWidget();
-    _routeMap["/Stack"] = (context) => TextsInStack();
-    _routeMap["/Login"] = (context) => LoginPage();
-    _routeMap["/Silver"] = (context) => SliverPage();
-    _routeMap["/Http"] = (context) => WebContentWidget();
-    _routeMap["/Bloc"] = (context) => CounterWithBloc();
-    _routeMap["/Provider"] = (context) => CounterWithProvider();
-    _routeMap["/Multi_Bloc"] = (context) => CounterWithMultiBloc();
-    _routeMap["/Lottie"] = (context) => LottieExampleWidget();
-    _routeMap["/Text_Overflow"] = (context) => TextOverflowWidget();
-    _routeMap["/"] =
-        (context) => HelloWorld(routeNames: _routeMap.keys.toList());
-  }
+class MyApp extends StatelessWidget {
+  final Map<String, WidgetBuilder> _routeMap = {
+    "/Bottom_Scroll": (context) => BottomScrollExample(),
+    "/Column_Flex": (context) => ColumnFlex(),
+    "/KeyListWidget": (context) => KeyListWidget(),
+    "/FloatingWidget": (context) => OverlayFloatingWidget(),
+    "/Get": (context) => GetCountView(),
+    "/Notification": (context) => MyNotificationWidget(),
+    "/Stack": (context) => TextsInStack(),
+    "/Login": (context) => LoginPage(),
+    "/Silver": (context) => SliverPage(),
+    "/Http": (context) => WebContentWidget(),
+    "/Bloc": (context) => CounterWithBloc(),
+    "/Provider": (context) => CounterWithProvider(),
+    "/Multi_Bloc": (context) => CounterWithMultiBloc(),
+    "/Lottie": (context) => LottieExampleWidget(),
+    "/Text_Overflow": (context) => TextOverflowWidget(),
+  };
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      routes: _routeMap,
+      routes: {
+        "/": (context) => HelloWorld(routeNames: _routeMap.keys.toList()),
+        ..._routeMap,
+      },
     );
   }
 }
