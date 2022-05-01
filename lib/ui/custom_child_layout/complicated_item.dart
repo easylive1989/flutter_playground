3526db28e8b7f79836cfe6414b42c66af098c1c7
Paul <easylive1989@gmail.com>
2022-05-01 17:17:45 +0800

    update

--- lib/ui/custom_child_layout/complicated_item.dart
+++ lib/ui/custom_child_layout/complicated_item.dart
@@ -1,15 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_playground/ui/custom_child_layout/user_info.dart';
 
 class UserList extends StatelessWidget {
   const UserList({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    List<UserInfo> userList = [
+      UserInfo(
+        avatar: "assets/nicolas.jpeg",
+        name: "Nicolas Cage",
+        aboutMe: "I never disrobe before gunplay.",
+        address: "Long Beach",
+        views: 9527,
+        stars: 141,
+      ),
+      UserInfo(
+        avatar: "assets/yao_ming.png",
+        name: "Yao Ming",
+        aboutMe: "I'm just one of the 1.3 billion Chinese.",
+        address: "China",
+        views: 9527,
+        stars: 141,
+      ),
+      UserInfo(
+        avatar: "assets/jacky_chan.jpeg",
+        name: "Jacky Chan",
+        aboutMe: "I only want my work to make people happy.",
+        address: "Hong Kong",
+        views: 9527,
+        stars: 141,
+      )
+    ];
     return Scaffold(
       body: ListView.builder(
-        itemCount: 10,
+        itemCount: userList.length,
         itemBuilder: (context, index) {
-          return UserInfoItem();
+          UserInfo userInfo = userList[index];
+          return UserInfoItem(userInfo: userInfo);
         },
       ),
     );
@@ -17,7 +45,12 @@ class UserList extends StatelessWidget {
 }
 
 class UserInfoItem extends StatelessWidget {
-  const UserInfoItem({Key? key}) : super(key: key);
+  final UserInfo userInfo;
+
+  const UserInfoItem({
+    Key? key,
+    required this.userInfo,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -33,83 +66,107 @@ class UserInfoItem extends StatelessWidget {
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  children: [
-                    Image.asset("assets/nicolas.jpeg", width: 60),
-                    Padding(
-                      padding: EdgeInsets.all(8),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            "Nicolas",
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                          SizedBox(height: 8),
-                          Text("I Never Disrobe Before Gunplay.", style: TextStyle(fontSize: 10)),
-                        ],
-                      ),
-                    )
-                  ],
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 8),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
                     children: [
-                      Row(
-                        children: [
-                          Icon(Icons.home_outlined, color: Colors.lightBlue),
-                          Text("Long Beach"),
-                        ],
-                      ),
-                      SizedBox(width: 8),
-                      Row(
-                        children: [
-                          Icon(Icons.remove_red_eye_outlined, color: Colors.lightBlue),
-                          Text("9527"),
-                        ],
-                      ),
-                      SizedBox(width: 8),
-                      Row(
-                        children: [
-                          Icon(Icons.star_border, color: Colors.lightBlue),
-                          Text("141"),
-                        ],
-                      ),
+                      Image.asset(userInfo.avatar, width: 60),
+                      Padding(
+                        padding: EdgeInsets.all(8),
+                        child: Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                userInfo.name,
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                              SizedBox(height: 8),
+                              Text(userInfo.aboutMe, style: TextStyle(fontSize: 10)),
+                            ],
+                          ),
+                        ),
+                      )
                     ],
                   ),
-                ),
-              ],
+                  Padding(
+                    padding: EdgeInsets.only(top: 8),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        UserTag(
+                          icon: Icons.home_outlined,
+                          value: userInfo.address.toString(),
+                        ),
+                        SizedBox(width: 8),
+                        UserTag(
+                          icon: Icons.remove_red_eye_outlined,
+                          value: userInfo.views.toString(),
+                        ),
+                        SizedBox(width: 8),
+                        UserTag(
+                          icon: Icons.star_border,
+                          value: userInfo.stars.toString(),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
             Column(
               children: [
-                SizedBox(
-                  width: 80,
-                  child: OutlinedButton(
-                    onPressed: () {},
-                    child: Text("VIEW", style: TextStyle(color: Colors.white)),
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all(Colors.lightBlue),
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  width: 80,
-                  child: OutlinedButton(
-                    onPressed: () {},
-                    child: Text("INVITE", style: TextStyle(color: Colors.white)),
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all(Colors.lightBlue),
-                    ),
-                  ),
-                ),
+                UserInfoButton(text: "VIEW"),
+                UserInfoButton(text: "INVITE"),
               ],
             )
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class UserTag extends StatelessWidget {
+  final IconData icon;
+  final String value;
+
+  const UserTag({
+    Key? key,
+    required this.icon,
+    required this.value,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Icon(icon, color: Colors.lightBlue),
+        Text(value),
+      ],
+    );
+  }
+}
+
+class UserInfoButton extends StatelessWidget {
+  final String text;
+
+  const UserInfoButton({
+    Key? key,
+    required this.text,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 80,
+      child: OutlinedButton(
+        onPressed: () {},
+        child: Text(text, style: TextStyle(color: Colors.white)),
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all(Colors.lightBlue),
         ),
       ),
     );
---
+++ lib/ui/custom_child_layout/user_info.dart
@@ -0,0 +1,17 @@
+class UserInfo {
+  final String avatar;
+  final String name;
+  final String aboutMe;
+  final String address;
+  final int views;
+  final int stars;
+
+  UserInfo({
+    required this.avatar,
+    required this.name,
+    required this.aboutMe,
+    required this.address,
+    required this.views,
+    required this.stars,
+  });
+}
